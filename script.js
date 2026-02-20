9973ff4b653665de55be85564fb828ca46329f32
yessir-gotit <218499181+yessir-gotit@users.noreply.github.com>
2026-02-20 15:47:35 +0600

    theme toggler

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("dorime");
+const toggleTheme = document.getElementById('theme-toggle');
+const html = document.documentElement;
+const toggleWrapper = document.querySelector('.theme-toggle-wrapper')
+
+// Loads THEME
+if (html.getAttribute('data-theme') === 'light'){
+    toggleTheme.checked = true;
+}
+
+// toggle theme animation
+toggleWrapper.classList.add('visible')
+
+// Toggle theme
+toggleTheme.addEventListener('change', () => {
+    if (toggleTheme.checked) {
+        html.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+    else {
+        html.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+})
+
